f34a37a60bf0652838e8f4cf1885804a0a62b952
Alexey Gavrilov <alexey.gavrilov@telenorlinx.com>
2025-04-06 11:52:15 +0200

    Update workflow

--- .github/scripts/update-staging.sh
+++ .github/scripts/update-staging.sh
@@ -9,7 +9,7 @@ git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
 git add -A
 git commit -F-<<EOF
-${{ github.event.head_commit.message }}
+${COMMIT_MESSAGE}
 
 Reference: ${GITHUB_SHA}
 EOF
