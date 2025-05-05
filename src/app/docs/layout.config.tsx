e6dd49da2dbe6af43419ec98904a9eec2195c2a2
Alexey Gavrilov <alexey.gavrilov@telenorlinx.com>
2025-05-05 22:32:15 +0200

    Outline documentation structure

--- src/app/docs/layout.config.tsx
+++ src/app/docs/layout.config.tsx
@@ -1,4 +1,6 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import Image from "next/image";
+import logo from "../../images/logo.svg";
 
 /**
  * Shared layout configurations
@@ -11,24 +13,10 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
+        <Image src={logo} width={24} height={24} alt="DriveConstructor logo" />
         Drive Constructor
       </>
     ),
   },
   themeSwitch: { enabled: false },
-  links: [
-    {
-      text: "Documentation",
-      url: "/docs",
-      active: "nested-url",
-    },
-  ],
 };
