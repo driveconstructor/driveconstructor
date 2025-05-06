05060464c9146b8fbf0dbcc6b14908682631c0e5
Alexey Gavrilov <alexey.gavrilov@telenorlinx.com>
2025-05-06 22:40:57 +0200

    Add test for CSS

---
+++ tests/page.spec.ts
@@ -0,0 +1,17 @@
+import { expect, test } from "@playwright/test";
+
+test("CSS is working", async ({ browserName, page }) => {
+  // for some reason CSS is not computed on firefox
+  test.skip(
+    browserName.toLowerCase() !== "chromium",
+    "Test only for chromium!",
+  );
+
+  await page.goto("/");
+  const label = page.getByRole("link", { name: "Drive Constructor" });
+  await label.hover();
+  const backgroundColor = await label.evaluate(
+    (l) => getComputedStyle(l).backgroundColor,
+  );
+  expect(backgroundColor).toBe("oklch(0.968 0.007 247.896)");
+});
