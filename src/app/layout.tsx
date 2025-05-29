402118b7f57f20139824d200e6960e8f9a97b199
Alexey Gavrilov <alexey.gavrilov@telenorlinx.com>
2025-05-29 10:18:56 +0200

    Add CSP policy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,22 @@ const inter = Inter({
   subsets: ["latin"],
 });
 
+// as suggested by next.js for static export output (script nonce is not possible)
+// and also disable external connection
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+    connect-src 'self';
+`;
+
 export default function RootLayout({
   children,
 }: {
@@ -12,6 +28,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
+      <head>
+        <meta httpEquiv="Content-Security-Policy" content={cspHeader} />
+      </head>
       <body>{children}</body>
     </html>
   );
