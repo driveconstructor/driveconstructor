225475d32edd9d2c46578f472865142afcedca6f
Alexey Gavrilov <alexey.gavrilov@telenorlinx.com>
2025-06-05 17:28:30 +0200

    Drop precision

--- src/model/trafo.tsx
+++ src/model/trafo.tsx
@@ -74,7 +74,6 @@ export const TrafoElement: SystemElement<Trafo> = {
       value: null,
       options: [null, ...Power],
       advanced: true,
-      precision: 1,
     },
     sideVoltageHV: {
       type: "number",
