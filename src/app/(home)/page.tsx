bf82226e97c3a74d105503ce1d88ea2a5a32269d
Alexey Gavrilov <alexey.gavrilov@telenorlinx.com>
2025-07-06 21:03:54 +0200

    Add links to youtube videos

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Applications from "./Applications";
 import PageTemplate from "./PageTemplate";
 
@@ -5,10 +6,32 @@ export default function Home() {
   return (
     <PageTemplate
       title="Welcome to DriveConstructor"
-      text="
-        Discover the beauty and complexity of the world of standard components.
-        Watch the complete list of tutorials on our YouTube channel.
-      "
+      text={
+        <>
+          Discover the beauty and complexity of the world of standard
+          components. Watch introduction videos for{" "}
+          <Link
+            href="https://www.youtube.com/watch?v=9fkdTpHpYyM"
+            className="underline"
+          >
+            professors
+          </Link>{" "}
+          and{" "}
+          <Link
+            href="https://www.youtube.com/watch?v=12ptSRRTnCY"
+            className="underline"
+          >
+            students{" "}
+          </Link>{" "}
+          along with the complete list of tutorials on our{" "}
+          <Link
+            href="https://www.youtube.com/@driveconstructor5112"
+            className="underline"
+          >
+            YouTube channel.
+          </Link>
+        </>
+      }
     >
       <Applications />
     </PageTemplate>
