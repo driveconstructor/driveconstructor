394c94b37c66cc6f09e721bed544d7c12db80d04
Alexey Gavrilov <alexey.gavrilov@telenorlinx.com>
2025-07-09 23:02:26 +0200

    Add LLM text for documentation

---
+++ src/app/docs/llm.txt/route.ts
@@ -0,0 +1,38 @@
+import type { InferPageType } from "fumadocs-core/source";
+import { remarkInclude } from "fumadocs-mdx/config";
+import { remark } from "remark";
+import remarkGfm from "remark-gfm";
+import remarkMdx from "remark-mdx";
+import { source } from "../source";
+
+const processor = remark()
+  .use(remarkMdx)
+  // needed for Fumadocs MDX
+  .use(remarkInclude)
+  .use(remarkGfm);
+
+async function getLLMText(page: InferPageType<typeof source>) {
+  const processed = await processor.process({
+    path: page.data._file.absolutePath,
+    value: page.data.content,
+  });
+
+  console.log(`Converting MDX to test: ${page.url}`);
+
+  return `# ${page.data.title}
+URL: ${page.url}
+
+${page.data.description}
+
+${processed.value}`;
+}
+
+// cached forever
+export const revalidate = false;
+
+export async function GET() {
+  const scan = source.getPages().map(getLLMText);
+  const scanned = await Promise.all(scan);
+
+  return new Response(scanned.join("\n\n"));
+}
